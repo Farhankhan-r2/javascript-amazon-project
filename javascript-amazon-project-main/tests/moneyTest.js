c1d69730526a78d6350cab413611db35c0e1125d
Farhan Khan <farhankhan.devv@gmail.com>
2026-03-27 15:45:49 +0530

    Added jasmine testing framework

---
+++ javascript-amazon-project-main/tests/moneyTest.js
@@ -0,0 +1,23 @@
+import { formatCurrency } from '../scripts/utils/money.js';
+
+//when creating test cases create basic as well as edge if any
+
+//this is just basic test case
+
+console.log('test suite: formatCurrency');
+console.log('converts cents into dollars');
+if (formatCurrency(2095) === '20.95') console.log('passed');
+else console.log('failed');
+
+
+//these two are edge cases
+console.log('works with zero');
+if (formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+};
+
+console.log('rounds  up to the nearest cents')
+if (formatCurrency(2000.5) === '20.01') console.log('passed');
+else console.log('failed');
